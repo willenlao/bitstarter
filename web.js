231b5259a8f296cdbf779cc9f95c9c3724b898d1
Willen Lao <willen-lao@cox.net>
2013-07-09 07:28:22 +0000

    Modified web.js 2

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(fs.readFileSync("index.html",'r'));
+    response.send(fs.readFileSync("index.html","utf8"));
 });
 
 var port = process.env.PORT || 5000;
